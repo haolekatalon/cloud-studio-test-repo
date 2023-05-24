7cb1765579f15a95966771cfd0d14924ca29d6ba
Hao <91060875+haolekatalon@users.noreply.github.com>
2023-05-24 10:36:00 +0700

    create test suite

---
+++ test-suites/tw-cloud-studio-CURA-test-suite.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>tw-cloud-studio-CURA-test-suite</name>
+    <testSuiteGuid>246ce76e-f560-42ca-b204-83ec0b9dbd82</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/tw-cloud-studio-tests/tw-g5-test</testCaseId>
+        <guid>3ec49fe7-0144-4fee-a8b1-028939d638c2</guid>
+    </testCaseLink>
+</TestSuiteEntity>
