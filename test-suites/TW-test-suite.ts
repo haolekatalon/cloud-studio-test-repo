1bfcf80df9eaf33f0df8305c46fa945b4067e693
Hao <91060875+haolekatalon@users.noreply.github.com>
2023-05-29 10:18:34 +0700

    created test suite

---
+++ test-suites/TW-test-suite.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>TW-test-suite</name>
+    <testSuiteGuid>1d65ef1a-b0ee-475c-bc22-799fbbd320d9</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/tw-cloud-studio-tests/tw-g5-test</testCaseId>
+        <guid>95dcb314-4c3d-46b6-9bbe-6ef03ccea717</guid>
+    </testCaseLink>
+</TestSuiteEntity>
